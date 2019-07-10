7caaa221fc6dbd6a8c58f749ff61b73aaeee113b
alce65 <alce65@hotmail.es>
2019-07-10 13:37:20 +0200

    Paso de JS a TS

--- ES6/02_funciones.js
+++ ES6/02_funciones.js
@@ -42,7 +42,7 @@ mostrar.apply(o3) // This es el objeto parametro, que "toma prestada la función
 
 // Uso de metodos como handlers
 
-/* setTimeout(o.mostrar, 1000)
+setTimeout(o.mostrar, 1000)
 setTimeout(o.mostrar.bind(o), 2000) */
  
 console.clear()
