618a0566947a9880a58ab571033a286b836a28a0
alce65 <alce65@hotmail.es>
2019-07-10 14:24:47 +0200

    Web completo

--- 08_controller.ts
+++ 08_controller.ts
@@ -1,18 +1,37 @@
+import { PAISES } from './08_maestros.js';
+
 interface INodos {
-    nombre : Element
-    output: Element
+    nombre : HTMLInputElement
+    btnBorrar: HTMLButtonElement
+    output: HTMLOutputElement
+    paises: HTMLSelectElement
 }
 
 export default class Controller {
     nodos: INodos;
+    aPaises: Array<string>;
     constructor() {
         console.log('Cargado el controller')
         this.nodos = { // aserción de tipos
             nombre: document.querySelector('#nombre') as HTMLInputElement ,
-            output: <Element>document.querySelector('#output')
+            btnBorrar: document.querySelector('#btnBorrar') as HTMLButtonElement,
+            output: <HTMLOutputElement>document.querySelector('#output'),
+            paises: <HTMLSelectElement>document.querySelector('#paises') 
         }
+        this.aPaises = PAISES
+
+        // Renderizado del DOM
+        let html = ''
+        this.aPaises.forEach(item => {
+            html += `<option>${item}</option>`
+        });
+        this.nodos.paises.innerHTML = html;
+        
+        // Manejadores de eventos
         this.nodos.nombre.addEventListener('input',
             this.onInput.bind(this) )
+        this.nodos.btnBorrar.addEventListener('click',
+            this.onClick.bind(this))
     }
 
     onInput(ev: Event) {
@@ -21,5 +40,10 @@ export default class Controller {
         console.log(this);
         console.dir(target);
         this.nodos.output.innerHTML =`Hola ${target.value}`
+    }
+
+    onClick() {
+        this.nodos.nombre.value = ''
+        this.nodos.output.innerHTML = ''
     }
 }
